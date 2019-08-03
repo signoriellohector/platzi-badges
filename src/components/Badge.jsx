1eef46858c8d857b06fdbe2c41c6273b15f76f43
Hector Signoriello <hector.signoriello@gmail.com>
2019-08-03 00:13:13 -0300

    estilo/Styles

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
+import '../styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <img src={confLogo} alt='Logo de la conferencia' />
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-        <div>
-          <img src='https://www.gravatar.com/avatar?d=identicon' alt='Avatar' />
-        </div>
-        <div>
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar?d=identicon"
+            alt="Avatar"
+          />
           <h1>
             Richard <br /> Kaufman
           </h1>
         </div>
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@asparragus</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@asparragus</div>
         </div>
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
