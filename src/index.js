755b8305a0d07ceff22fc32b7ac555f6f472467f
Hector Signoriello <hector.signoriello@gmail.com>
2019-08-02 21:34:18 -0300

    jsx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// const jsx = <h1>Hello, platzi Badges!</h1>;
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://platzi.com' },
+//   'Ir a platzi'
+// );
+// const name = 'angela';
+// const sum = () => 3 + 3;
+// const jsx = <h1>Hola soy, {undefined}</h1>;
+// const element = React.cr+eateElement('h1', {}, `Hola, soy ${name}`);
+const jsx = (
+  <div>
+    <h1>Hola, soy Richard</h1>
+    <p>Soy ingeniero Frontend.</p>
+  </div>
+);
 
-const element = <h1>Hello, platzi Badges from React!</h1>;
-
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, 'Hola, soy Richard'),
+  React.createElement('p', {}, 'Soy ingeniero de la Web.')
+);
 const container = document.getElementById('app');
 
 // ReactDOM.render (__qué__, __dónde__);
